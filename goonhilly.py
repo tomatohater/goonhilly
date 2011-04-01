764742e0c88edfe8a824860c0fc24b376a76659c
Drew Engelson <drew@engelson.net>
2011-04-01 16:20:44 -0400

    Added authorized apps to settings.

--- goonhilly.py
+++ goonhilly.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -10,7 +10,7 @@ import logging.handlers
 
 logger = logging.getLogger('Goonhilly')
 logger.setLevel(logging.INFO)
-log_handler = logging.FileHandler(app.config['GOONHILLY_LOG'])
+log_handler = logging.FileHandler(app.config['GOONHILLY_LOG_FILE'])
 log_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s [%(levelname)s] %(message)s"))
 logger.addHandler(log_handler)
 
@@ -21,11 +21,23 @@ def clean(s):
     return s
 
 
-@app.route('/<sourceapp>/', methods=['POST', 'GET',])
-def log():
+@app.route('/', methods=['GET',])
+def index():
+    return '<style type="text/css">' \
+           '    html, body { height: 100%; margin: 0; padding: 0; }' \
+           '    img#bg { position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: -1; }' \
+           '</style>' \
+           '<img src="http://www.sciencephoto.com/images/showFullWatermarked.html/C0015325-Goonhilly_Satellite_Earth_Station,_UK-SPL.jpg?id=670015325" id="bg" />', 200
+
+
+@app.route('/<source_tag>/', methods=['POST', 'GET',])
+def log(source_tag):
+    if source_tag not in app.config['GOONHILLY_AUTHORIZED_SOURCE_TAGS']:
+        return abort(401)
+    
     l = ['%s=%s' % (clean(k), clean(v)) for k, v in request.values.iteritems()]
     l.append('%s=%s' % (clean('client_id'), clean(request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR') or '-')))
-    l.append('%s=%s' % (clean('sourceapp'), clean(sourceapp)))
+    l.append('%s=%s' % (clean('source_tag'), clean(source_tag)))
     out = ' '.join(l)
     logger.info(out)
     return 'CREATED', 201
--- settings.py
+++ settings.py
@@ -1,3 +1,13 @@
+#
+# Default Flask settings
+#
 DEBUG = False
 SECRET_KEY = 'A_UNIQUE_KEY_FOR_YOUR_APPLICATION'
-GOONHILLY_LOG = 'PATH_TO_LOGFILE'
+
+
+#
+# Default Goonhilly settings
+#
+GOONHILLY_LOG_FILE = 'PATH_TO_LOGFILE'
+GOONHILLY_AUTHORIZED_SOURCE_TAGS = ['SOURCE_TAG_1',
+                                    'SOURCE_TAG_2', ]
